b69526990ea28b3c52469d73fee675291bf7c881
Hema Sai Reddy <158816242+BunnyHemaSaiReddy@users.noreply.github.com>
2024-08-27 16:35:38 +0530

    Update bunny_file.py

--- bunny_file.py
+++ bunny_file.py
@@ -6,7 +6,6 @@ from moviepy.editor import VideoFileClip
 import PIL.Image as Image
 import speech_recognition as sr
 import streamlit as st
-import tempfile
 import assemblyai as aai
 
 def transcribe_audio(audio_file_path):
@@ -52,17 +51,17 @@ def process_file(uploaded_file):
         text = df.to_string()
         return text, 'excel'
     
-    elif file_type.startswith('video/'):
-        #st.video(uploaded_file)
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video_file:
-            temp_video_file.write(uploaded_file.getbuffer())
-            temp_video_file_path = temp_video_file.name
-        video = VideoFileClip(temp_video_file_path)
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
-            audio_path = temp_audio_file.name
-            video.audio.write_audiofile(audio_path)
-        transcript = transcribe_audio(audio_path)
-        return transcript, 'video'
+    # elif file_type.startswith('video/'):
+    #     #st.video(uploaded_file)
+    #     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video_file:
+    #         temp_video_file.write(uploaded_file.getbuffer())
+    #         temp_video_file_path = temp_video_file.name
+    #     video = VideoFileClip(temp_video_file_path)
+    #     with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
+    #         audio_path = temp_audio_file.name
+    #         video.audio.write_audiofile(audio_path)
+    #     transcript = transcribe_audio(audio_path)
+    #     return transcript, 'video'
     
     elif file_type.startswith('audio/'):
         #st.audio(uploaded_file)
